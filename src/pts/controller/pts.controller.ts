6aa87bad2dbbfac3b2616d512c6b223bd7340ec7
Joaquin <joaquin.fumeaux@sofka.com.co>
2023-04-04 09:26:07 -0300

    agregamos funcion a controlador pts

--- src/pts/controller/pts.controller.ts
+++ src/pts/controller/pts.controller.ts
@@ -1,34 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { PtsService } from '../service/pts.service';
-import { CreatePtDto } from '../dto/create-pt.dto';
-import { UpdatePtDto } from '../dto/update-pt.dto';
 
 @Controller('pts')
 export class PtsController {
   constructor(private readonly ptsService: PtsService) {}
 
-  @Post()
-  create(@Body() createPtDto: CreatePtDto) {
-    return this.ptsService.create(createPtDto);
+  @Post(':identificationId/')
+  async sendInfoTransfer(@Param('id') identificationId: string, @Body('transactionAmount') transactionAmount: string){
+    return await this.ptsService.sendInfoTransfer(identificationId, transactionAmount)
   }
-
-  @Get()
-  findAll() {
-    return this.ptsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ptsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePtDto: UpdatePtDto) {
-    return this.ptsService.update(+id, updatePtDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ptsService.remove(+id);
-  }
+  
 }
