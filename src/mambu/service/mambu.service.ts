6da2596eae673becbbfc26467f25758fd3e12e43
Joaquin <joaquin.fumeaux@sofka.com.co>
2023-04-04 09:51:13 -0300

    headers para las peticiones

--- src/mambu/service/mambu.service.ts
+++ src/mambu/service/mambu.service.ts
@@ -2,22 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from "@nestjs/config";
 import { UserInfo } from '../dto/UserInfo';
 import { AccountInfo } from '../dto/AccountInfo';
+
 @Injectable()
 export class MambuService {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) { }
 
   // Obtenemos un cliente por ID
-  async getClientById(clientId: string) : Promise<UserInfo> {
-    clientId = '445345901'
+  async getClientById(clientId: string): Promise<UserInfo> {
     const urlMambu = this.configService.get<string>("URL_MAMBU") + "/clients/" + clientId;
-    return await fetch(urlMambu).then((res) => res.json());
+    return await fetch(urlMambu, {
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',
+        Accept: 'application/vnd.mambu.v2+json',
+        Authorization:
+          'Basic ' + window.btoa(this.configService.get<string>("USER_MAMBU") + ':' + this.configService.get<string>("PWD_MAMBU")),
+      },
+    }).then((res) => res.json());
   }
 
   // Obtenemos la cuenta de un cliente
-  async getAccountById(accountId: string) : Promise<AccountInfo>{
-    accountId = '99887766'
+  async getAccountById(accountId: string): Promise<AccountInfo> {
     const urlMambu = this.configService.get<string>("URL_MAMBU") + "/deposits/" + accountId
-    return await fetch(urlMambu).then((res) => res.json());
+    return await fetch(urlMambu, {
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',
+        Accept: 'application/vnd.mambu.v2+json',
+        Authorization:
+          'Basic ' + window.btoa(this.configService.get<string>("USER_MAMBU") + ':' + this.configService.get<string>("PWD_MAMBU")),
+      }
+    }).then((res) => res.json());
   }
 
 }
