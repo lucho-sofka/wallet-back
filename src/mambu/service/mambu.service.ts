60697ad95366c46cc96784b9b873d9e2f1df9314
lucho-sofka <luciano.sofka@sofka.com.co>
2023-04-12 10:49:54 -0300

    Se corrige getAllClientData

--- src/mambu/service/mambu.service.ts
+++ src/mambu/service/mambu.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from "@nestjs/config";
 import { UserInfo } from "../dto/UserInfo";
 import { AccountInfo } from "../dto/AccountInfo";
 import { UserData } from "../dto/UserData";
-import cli from "@angular/cli";
+import { log } from "console";
 
 @Injectable()
 export class MambuService {
@@ -52,33 +52,46 @@ export class MambuService {
   }
 
   //Obtener todos los datos del usuario a partir de su ID.
-  async getAllClientData(clientId: string){
-    const urlMambu = this.configService.get<string>("URL_MAMBU") + "/clients/" + clientId + "/savings"
+  async getAllClientData(clientId: string) {
+    const urlMambu =
+      this.configService.get<string>("URL_MAMBU") +
+      "/clients/" +
+      clientId +
+      "/savings";
     //Obtengo las cuentas del cliente
     const clientAccounts = await fetch(urlMambu, {
       headers: {
         "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': '*',
-        Accept: 'application/vnd.mambu.v2+json',
+        "Access-Control-Allow-Origin": "*",
+        Accept: "*/*",
         Authorization:
-          'Basic ' + window.btoa(this.configService.get<string>("USER_MAMBU") + ':' + this.configService.get<string>("PWD_MAMBU")),
-      }
+          "Basic " +
+          Buffer.from(
+            this.configService.get<string>("USER_MAMBU") +
+              ":" +
+              this.configService.get<string>("PWD_MAMBU"),
+            "utf8"
+          ).toString("base64"),
+      },
     }).then((res) => res.json());
+    console.log(clientAccounts);
 
     //Obtengo cliente
     const client = await this.getClientById(clientId);
 
     //Filtro las cuentas del cliente por el nombre (también puede ser por id) así este filtro no se hace desde el front
-    const requiredAccount = clientAccounts.filter((account) => account.name === "CTE_Sofka")[0];
-    
+    const requiredAccount = clientAccounts.filter(
+      (account) => account.name === "CTE_Sofka"
+    )[0];
+
     //Creo el objeto solamente con los datos que se precisan desde el front.
     const userData: UserData = {
       fullName: client.firstName + client.lastName,
       userAccount: {
         balance: requiredAccount.balance,
-        availableBalance: requiredAccount.availableBalance
-      }
-    } 
+        availableBalance: requiredAccount.availableBalance,
+      },
+    };
 
     return userData;
   }
